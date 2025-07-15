8cadc35da20a7d0267f9caf5a97383c65de6f16a
badweathermusic <uriel@Mac-mini-de-Uriel.local>
2025-07-15 00:34:22 -0600

    area de rectangulo

---
+++ area-rectangulo.py
@@ -0,0 +1,11 @@
+"""
+Calcular el área de un rectangulo.
+Formula: area = base x altura
+"""
+
+ancho = int(input("Ingrese la base del rectangulo: "))
+altura = int(input("Ingrese la altura del rectangulo: "))
+
+area = ancho*altura
+
+print(f"El area del rectangulo es {area}")
